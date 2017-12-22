4be2a4153dfd693203595ed64d9187c6438e5296
ychamedes <34750353+ychamedes@users.noreply.github.com>
2017-12-22 10:22:08 +0200

    Add files via upload

---
+++ torpedo.py
@@ -0,0 +1,12 @@
+##############################################################################
+# FILE : torpedo.py
+# WRITER : Jason Greenspan, jasonmg, 336126362; Yonatan Chamudot, ychamudot,
+#  312516289
+# EXERCISE : intro2cs ex9 2017-2018
+# DESCRIPTION: Creates the torpedoes for asteroids game.
+##############################################################################
+from moveable_object import MoveableObject
+
+Class Torpedo(MoveableObject):
+    def __init__(self):
+        MoveableObject.__init__(self)
