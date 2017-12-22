f0461dcc830e317244a5cecf55b798fffcb85dd9
ychamedes <34750353+ychamedes@users.noreply.github.com>
2017-12-22 12:18:40 +0200

    Update asteroids_main.py

--- asteroids_main.py
+++ asteroids_main.py
@@ -1,6 +1,7 @@
 ##############################################################################
 # FILE : asteroids_main.py
-# WRITER : Jason Greenspan, jasonmg, 336126362; Yonatan Chamudot, ychamudot, 312516289
+# WRITER : Jason Greenspan, jasonmg, 336126362; Yonatan Chamudot, ychamudot,
+#  312516289
 # EXERCISE : intro2cs ex9 2017-2018
 # DESCRIPTION: Runs the asteroid game.
 ##############################################################################
@@ -9,14 +10,15 @@ import math
 from screen import Screen
 from ship import Ship
 from asteroid import Asteroid
+from torpedo import Torpedo
 
 DEFAULT_ASTEROIDS_NUM = 5
 CRASH_TITLE = 'You crashed!'
 CRASH_MESSAGE = 'You lost a life'
 TURN_DEG = 7
 
-class GameRunner:
 
+class GameRunner:
     def __init__(self, asteroids_amnt):
         self.ship = Ship()
         self._screen = Screen()
@@ -26,6 +28,7 @@ class GameRunner:
         self.screen_min_x = Screen.SCREEN_MIN_X
         self.screen_min_y = Screen.SCREEN_MIN_Y
         self.asteroids = []
+        self.torpedoes = []
 
         for asteroid in range(asteroids_amnt):
             current_ast = Asteroid()
@@ -34,7 +37,7 @@ class GameRunner:
             Screen.draw_asteroid(self._screen, current_ast, Asteroid.get_x(
                 current_ast), Asteroid.get_y(current_ast))
             self.asteroids.append(current_ast)
-            
+
     def run(self):
         self._do_loop()
         self._screen.start_screen()
@@ -48,7 +51,7 @@ class GameRunner:
 
         # Set the timer to go off again
         self._screen.update()
-        self._screen.ontimer(self._do_loop,5)
+        self._screen.ontimer(self._do_loop, 5)
 
     def _game_loop(self):
         if Screen.is_left_pressed(self._screen):
@@ -59,29 +62,44 @@ class GameRunner:
             new_x_speed = self.ship.get_x_speed() + math.cos(
                 math.radians(self.ship.get_heading()))
             new_y_speed = self.ship.get_y_speed() + math.sin(
-                math.radians(self.ship.get_heading()))   
+                math.radians(self.ship.get_heading()))
             self.ship.set_x_speed(new_x_speed)
             self.ship.set_y_speed(new_y_speed)
         self.ship.move()
         Screen.draw_ship(self._screen, self.ship.get_x(), self.ship.get_y(),
                          self.ship.get_heading())
-        
+
         for asteroid in self.asteroids:
             asteroid.move()
-            self._screen.draw_asteroid(asteroid, asteroid.get_x(), asteroid.get_y())
+            self._screen.draw_asteroid(asteroid, asteroid.get_x(),
+                                       asteroid.get_y())
             if asteroid.has_intersection(self.ship):
                 Screen.show_message(self, CRASH_TITLE, CRASH_MESSAGE)
                 self._screen.remove_life()
                 self._screen.unregister_asteroid(asteroid)
                 self.asteroids.remove(asteroid)
 
+        if Screen.is_space_pressed(self._screen):
+            current_tor = Torpedo()
+            Screen.register_torpedo(self._screen, current_tor)
+            Screen.draw_torpedo(self._screen, current_tor, Torpedo.get_x(
+                current_tor), Torpedo.get_y(current_tor),
+                                 Torpedo.get_heading(current_tor))
+            self.torpedoes.append(current_tor)
+
+        for torpedo in self.torpedoes:
+            torpedo.move()
+            self._screen.draw_torpedo(torpedo, torpedo.get_x(),
+                                       torpedo.get_y(), torpedo.get_heading())
+
 
 def main(amnt):
     runner = GameRunner(amnt)
     runner.run()
 
+
 if __name__ == "__main__":
     if len(sys.argv) > 1:
-        main( int( sys.argv[1] ) )
+        main(int(sys.argv[1]))
     else:
-        main( DEFAULT_ASTEROIDS_NUM )
+        main(DEFAULT_ASTEROIDS_NUM)
