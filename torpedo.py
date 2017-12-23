2dab420c8a21b97ebc8105da5ab4f78d87de3fc3
ychamedes <34750353+ychamedes@users.noreply.github.com>
2017-12-23 19:57:44 +0200

    Update torpedo.py

--- torpedo.py
+++ torpedo.py
@@ -8,11 +8,25 @@
 from moveable_object import MoveableObject
 
 INITIAL_HEADING = 0.0
+TORPEDO_RADIUS = 4
+TORPEDO_SPEED = 10
 
 class Torpedo(MoveableObject):
     def __init__(self):
         MoveableObject.__init__(self)
         self.__heading = INITIAL_HEADING
+        self.__radius = TORPEDO_RADIUS
+        self.set_x_speed(TORPEDO_SPEED)
+        self.set_y_speed(TORPEDO_SPEED)
 
     def get_heading(self):
         return self.__heading
+
+    def get_radius(self):
+        return self.__radius
+
+    def set_heading(self, heading):
+        self.__heading = heading
+
+    def new_tor_speed_x(self):
+        pass
