aeeb72377bbf826a1789d15d1d130609b384600f
jasgreenspan <34744366+jasgreenspan@users.noreply.github.com>
2017-12-24 14:25:25 +0200

    Update ship.py

--- ship.py
+++ ship.py
@@ -9,6 +9,7 @@ from moveable_object import MoveableObject
 
 INITIAL_HEADING = 0.0
 SHIP_RADIUS = 1
+MAX_SPEED = 5
 
 class Ship(MoveableObject):
     def __init__(self):
@@ -24,3 +25,11 @@ class Ship(MoveableObject):
 
     def get_radius(self):
         return self.__radius
+    
+    def set_ship_x_speed(self, x_speed):
+        if x_speed <= MAX_SPEED and x_speed >= -MAX_SPEED:  
+            self.set_x_speed(x_speed)
+    
+    def set_ship_y_speed(self, y_speed):
+        if y_speed <= MAX_SPEED and y_speed >= -MAX_SPEED:
+            self.set_y_speed(y_speed)
