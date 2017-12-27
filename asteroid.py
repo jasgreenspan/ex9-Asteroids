30060303419a101be3480ee3eb3458b25d40b3c6
ychamedes <34750353+ychamedes@users.noreply.github.com>
2017-12-27 21:01:27 +0200

    Update asteroid.py

--- asteroid.py
+++ asteroid.py
@@ -12,13 +12,15 @@ RADIUS_FACTOR = 10
 RADIUS_CORRECTER = 5
 ASTEROID_INITIAL_RADIUS = 3
 MIN_ASTEROID_SPEED = 1
-MAX_ASTEROID_SPEED = 3
+MAX_ASTEROID_SPEED = 2
 POSITIVE_DIRECTION = 1
 NEGATIVE_DIRECTION = -1
 
+
 class Asteroid(MoveableObject):
     def __init__(self):
-        # Give asteroid random speed and direction.
+        """Initiate the asteroid class. The asteroid's initial position and
+        speed are determined randomly, while its initial size is a constant"""
         x_speed = random.randint(MIN_ASTEROID_SPEED, MAX_ASTEROID_SPEED) * \
                   random.choice((NEGATIVE_DIRECTION, POSITIVE_DIRECTION))
         y_speed = random.randint(MIN_ASTEROID_SPEED, MAX_ASTEROID_SPEED) * \
@@ -27,15 +29,21 @@ class Asteroid(MoveableObject):
         self.__size = ASTEROID_INITIAL_RADIUS
 
     def get_size(self):
+        """Returns the size of the current asteroid"""
         return self.__size
 
     def set_size(self, size):
+        """Sets the size of the current asteroid"""
         self.__size = size
 
     def get_radius(self):
+        """Returns the radius of the current asteroid (a function of its
+        size and constant factors)"""
         return (self.__size * RADIUS_FACTOR) - RADIUS_CORRECTER
 
     def has_intersection(self, obj):
+        """Returns True if the current asteroid crashes into another object,
+        and False otherwise"""
         intersection = False
         # Calcutate distance between asteroid and intersecting object.
         distance = math.sqrt(((obj.get_x() - self.get_x()) ** 2) + (
