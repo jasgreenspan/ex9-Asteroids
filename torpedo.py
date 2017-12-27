5b4b95bd34dc61db18a025c4cd3ffc8e5f87996e
ychamedes <34750353+ychamedes@users.noreply.github.com>
2017-12-27 21:05:36 +0200

    Update torpedo.py

--- torpedo.py
+++ torpedo.py
@@ -12,29 +12,39 @@ TORPEDO_RADIUS = 4
 ACCELERATION_FACTOR = 2
 INITIAL_LIFESPAN = 0
 
+
 class Torpedo(MoveableObject):
     def __init__(self):
+        """Initiates the torpedo class"""
         MoveableObject.__init__(self)
         self.__radius = TORPEDO_RADIUS
         self.__lifespan = INITIAL_LIFESPAN
 
     def get_heading(self):
+        """Returns the torpedo's heading"""
         return self.__heading
 
     def get_radius(self):
+        """Returns the torpedo's radius"""
         return self.__radius
 
     def set_heading(self, heading):
+        """Sets the torpedo's heading"""
         self.__heading = heading
 
     def set_torpedo_speed(self):
+        """Sets the x and y speed of the torpedo"""
         self.set_x_speed(self.get_x_speed() + (ACCELERATION_FACTOR *
-                         math.cos(math.radians(self.get_heading()))))
+                                               math.cos(math.radians(
+                                                   self.get_heading()))))
         self.set_y_speed(self.get_y_speed() + (ACCELERATION_FACTOR *
-                         math.sin(math.radians(self.get_heading()))))
+                                               math.sin(math.radians(
+                                                   self.get_heading()))))
 
     def get_lifespan(self):
+        """Returns the lifespan of the torpedo"""
         return self.__lifespan
 
     def get_older(self):
+        """Increases the lifespan of the torpedo by one"""
         self.__lifespan += 1
