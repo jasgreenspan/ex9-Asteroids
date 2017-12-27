ebe154cd699cd439d813492931a358e56fbfad4b
ychamedes <34750353+ychamedes@users.noreply.github.com>
2017-12-27 21:03:58 +0200

    Update ship.py

--- ship.py
+++ ship.py
@@ -11,27 +11,32 @@ INITIAL_HEADING = 0.0
 SHIP_RADIUS = 1
 MAX_SPEED = 10
 
+
 class Ship(MoveableObject):
     def __init__(self):
+        """Initiates the ship class"""
         MoveableObject.__init__(self)
         self.__heading = INITIAL_HEADING
         self.__radius = SHIP_RADIUS
 
     def get_heading(self):
+        """Returns the heading of the ship"""
         return self.__heading
 
     def set_heading(self, heading):
+        """Sets the heading of the ship"""
         self.__heading = heading
 
     def get_radius(self):
+        """Returns the radius of the ship"""
         return self.__radius
-    
+
     def set_ship_x_speed(self, x_speed):
-        # Only update x_speed if new speed is below limit (in both directions).
-        if x_speed <= MAX_SPEED and x_speed >= -MAX_SPEED:  
+        """Sets the speed of the ship along the x axis"""
+        if x_speed <= MAX_SPEED and x_speed >= -MAX_SPEED:
             self.set_x_speed(x_speed)
-    
+
     def set_ship_y_speed(self, y_speed):
-        # Only update y_speed if new speed is below limit (in both directions).
+        """Sets the speed of the ship along the y axis"""
         if y_speed <= MAX_SPEED and y_speed >= -MAX_SPEED:
             self.set_y_speed(y_speed)
